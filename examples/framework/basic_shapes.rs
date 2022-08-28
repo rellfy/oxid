fbb6008e60934895c82818a9445c37ab1dbaa794
Lorenzo <lorenzo@nioel.com>
2022-08-29 00:40:24 +1000

    update basic_shapes example

--- examples/framework/basic_shapes.rs
+++ examples/framework/basic_shapes.rs
@@ -1,16 +1,17 @@
 use oxid_framework::prelude::*;
 
-#[oxid_framework::main("BasicShapes")]
+#[rwasm::main]
 async fn main() {
-    loop {
-        clear_background(RED);
+    oxid_framework::Window::new("basic_shapes", render());
+}
 
+async fn render() {
+    loop {
+        clear_background(BLACK);
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        draw_text("HELLO", 20.0, 20.0, 30.0, DARKGRAY);
-
+        draw_text("HELLO", 20.0, 20.0, 30.0, WHITE);
         next_frame().await
     }
 }
